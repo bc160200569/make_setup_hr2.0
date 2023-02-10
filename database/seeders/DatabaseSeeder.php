200f3770cb6c844a35284feedaf3a9efb0d50c3d
bc160200569 <118533654+bc160200569@users.noreply.github.com>
2023-02-10 18:24:32 +0500

    Add seeder class in db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -15,11 +16,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::create([
-            'username' => 'super_admin',
-            'name' => 'Super Admin', 
-            'email' => 'admin@pitb.gov.pk', 
-            'password' => Hash::make('admin@pitb')
+        $this->call([
+            RoleSeeder::class,
+            SuperAdminSeeder::class,
+            NavigationSeeder::class,
         ]);
+        // Role::create([
+        //     'name' => 'super_admin',
+        //     'is_active' => '1',
+        // ]);
+        // User::create([
+        //     'username' => 'super_admin',
+        //     'name' => 'Super Admin', 
+        //     'email' => 'admin@pitb.gov.pk', 
+        //     'password' => Hash::make('admin@pitb'),
+        //     'phone_number' => '0000000000',
+        //     'role_id' => '1',
+        //     'is_active' => '1',
+        //     'created_by' => '0',
+        //     'updated_by' => '0',
+        // ]);
     }
 }
