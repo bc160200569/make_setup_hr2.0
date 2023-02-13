fbe44f6c2b062e57c1edea476d45ac280d909331
bc160200569 <118533654+bc160200569@users.noreply.github.com>
2023-02-13 18:26:01 +0500

    Add scope query for uniqe id

--- app/Models/Navigation.php
+++ app/Models/Navigation.php
@@ -10,6 +10,40 @@ class Navigation extends Model
 {
     use HasFactory;
 
+    /**
+     * Scope a query to only include active users.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeUuid($query, $uuid)
+    {
+        return $query->where('uuid', $uuid);
+    }
+    
+    /**
+    * Get the route key for the model.
+    *
+    * @return string
+    */
+    public function getRouteKeyName()
+    {
+        return 'uuid';
+    }
+
+    /**
+     * boot
+     */
+    protected static function boot ()
+    {
+    	parent::boot();
+
+        static::creating(function ($model) {
+            $model->uuid = getUuid();
+        });
+        
+    }
+
 
     public function navrole()
     {
