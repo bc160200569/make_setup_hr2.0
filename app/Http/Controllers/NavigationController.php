7a5ffe5a464ddd93b3fc4c0ce7a0d95b4ca98bd4
bc160200569 <118533654+bc160200569@users.noreply.github.com>
2023-02-15 18:31:31 +0500

    make view and controller for bootstrap model

--- app/Http/Controllers/NavigationController.php
+++ app/Http/Controllers/NavigationController.php
@@ -6,13 +6,69 @@ use App\Models\Navigation;
 use Illuminate\Http\Request;
 
 use DataTables;
+use Session;
 
 
 class NavigationController extends Controller
 {
     //
-    public function index(Request $request)
-    {
+    // public function index(Request $request)
+    // {
+    //     if ($request->ajax()) {
+            
+    //         $navigations = Navigation::query();
+    //         // dd($navigations);
+
+    //         return Datatables::of($navigations)
+    //             ->editColumn('sub_nav', function ($nav){
+    //                 if ($nav->sub_nav == 0) return 'No';
+    //                 if ($nav->sub_nav == 1) return 'Yes';
+    //                 // return 'Cancel';
+    //             })
+    //             ->editColumn('is_show', function ($nav){
+    //                 if ($nav->is_show == 0) return 'No';
+    //                 if ($nav->is_show == 1) return 'Yes';
+    //                 // return 'Cancel';
+    //             })
+    //             ->editColumn('route', function ($nav){
+    //                 if ($nav->route == '') return 'No Route';
+    //                 if ($nav->route != '') return 'route';
+    //                 // return 'Cancel';
+    //             })
+    //             ->addColumn('action', function ($nav) {
+    //                 $statusAction = '<td>
+    //                                     <span class="badge '.($nav->is_active == 1 ? "bg-success" : "bg-danger").'">'.($nav->is_active == 1 ? "Active" : "Inactive").'</span>
+    //                                     <div class="overlay-edit">
+    //                                         <a href="'.route('users.edit', $nav->uuid).'" class="btn btn-icon btn-secondary"><i class="feather icon-edit-2"></i></a>
+    //                                         <a href="'.route('users.updateStatus', $nav->uuid).'" class="btn btn-icon '.($nav->is_active == 1 ? "btn-danger" : "btn-success").' btn-status"><i class="feather '.($nav->is_active == 1 ? "icon-eye-off" : "icon-eye").'"></i></a>
+    //                                         <a href="'.route('users.destroy', $nav->uuid).'" class="btn btn-icon btn-danger btn-delete"><i class="feather icon-trash-2"></i></a>
+    //                                     </div>
+    //                                 </td>';
+    //                 return $statusAction;
+    //             })
+    //             ->addColumn('sub_navigation', function ($nav){
+    //                 $sub = '<td>
+    //                             <span class="badge '.($nav->sub_nav == 1 ? "bg-primary" : "bg-danger").'">'.($nav->sub_nav == 1 ? "View Sub-Navigation" : "Add Sub-Navigation").'</span>
+    //                             <div class="overlay-edit">
+    //                                 <a href="'.route('users.edit', $nav->uuid).'" class="btn btn-icon btn-secondary"><i class="feather icon-list"></i></a>
+    //                             </div>
+    //                         </td>';
+    //                 return $sub;
+    //             })
+    //             ->editColumn('icon', function ($nav){
+    //                 $icon = '<td>
+    //                             <i class="'.($nav->icon).'"></i>
+    //                         </td>';
+    //                 return $icon;
+    //             })
+    //             ->editColumn('id', 'ID: {{$id}}')
+    //             ->rawColumns(['action', 'sub_navigation', 'icon'])
+    //             ->make(true);
+    //     }
+        
+    //     return view('navigations.index');
+    // }
+    public function navigation_index(Request $request){
         if ($request->ajax()) {
             
             $navigations = Navigation::query();
@@ -37,18 +93,57 @@ class NavigationController extends Controller
                 ->addColumn('action', function ($nav) {
                     $statusAction = '<td>
                                         <span class="badge '.($nav->is_active == 1 ? "bg-success" : "bg-danger").'">'.($nav->is_active == 1 ? "Active" : "Inactive").'</span>
-                                        <div class="overlay-edit">
-                                            <a href="'.route('users.edit', $nav->uuid).'" class="btn btn-icon btn-secondary"><i class="feather icon-edit-2"></i></a>
-                                            <a href="'.route('users.updateStatus', $nav->uuid).'" class="btn btn-icon '.($nav->is_active == 1 ? "btn-danger" : "btn-success").' btn-status"><i class="feather '.($nav->is_active == 1 ? "icon-x-circle" : "icon-check-circle").'"></i></a>
+                                        <div class="overlay-edit">                                        
+                                            <button type="button" value="'.$nav->uuid.'" class="btn btn-icon btn-secondary" id="editNavigation"><i class="feather icon-edit-2"></i></button>
+                                            
+                                            <a href="'.route('users.updateStatus', $nav->uuid).'" class="btn btn-icon '.($nav->is_active == 1 ? "btn-danger" : "btn-success").' btn-status"><i class="feather '.($nav->is_active == 1 ? "icon-eye-off" : "icon-eye").'"></i></a>
                                             <a href="'.route('users.destroy', $nav->uuid).'" class="btn btn-icon btn-danger btn-delete"><i class="feather icon-trash-2"></i></a>
                                         </div>
                                     </td>';
                     return $statusAction;
                 })
+                ->addColumn('sub_navigation', function ($nav){
+                    $sub = '<td>
+                                <span class="badge '.($nav->sub_nav == 1 ? "bg-primary" : "bg-danger").'">'.($nav->sub_nav == 1 ? "View Sub-Navigation" : "Add Sub-Navigation").'</span>
+                                <div class="overlay-edit">
+                                    <a href="'.route('users.edit', $nav->uuid).'" class="btn btn-icon btn-secondary"><i class="feather icon-list"></i></a>
+                                </div>
+                            </td>';
+                    return $sub;
+                })
+                ->editColumn('icon', function ($nav){
+                    $icon = '<td>
+                                <i class="'.($nav->icon).'"></i>
+                            </td>';
+                    return $icon;
+                })
                 ->editColumn('id', 'ID: {{$id}}')
+                ->rawColumns(['action', 'sub_navigation', 'icon'])
                 ->make(true);
         }
-        
-        return view('navigations.index');
+        return view('navigations.navigation_index');
+    }
+    public function store(Request $request){
+        request()->validate([
+            'name' => 'required',
+            ],
+            // [
+            //     'name.required'=>"Navigation not add Name Field Required.",
+            // ]
+        );
+        // dd($request->all());
+        Navigation::create($request->all());
+        Session::flash('success', __('messages.Navigation_created'));
+
+        return redirect()->route('navigation_index');
+    }
+    public function navigation_edit($id){
+        $navigation = Navigation::where('uuid', $id)->get();
+        // dd($navigation);
+        // dd($id);
+        return response()->json([
+            'status' => 200,
+            'navigation' => $navigation,
+        ]);
     }
 }
---
+++ resources/views/navigations/navigation_index.blade.php
@@ -0,0 +1,301 @@
+<x-app-layout>
+    <div class="container">
+        <div class="modal" id="add_navigation">
+            <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+
+                    @php
+                    $title = "Add Navigation";
+                    @endphp
+                    @section('title')
+                    {{ $title }}
+                    @endsection
+                    {{--<!-- Modal Header -->--}}
+                    <div class="modal-header">
+                        <h4 class="modal-title">{{ $title }}</h4>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                    </div>
+                    {{--<!-- Modal body -->--}}
+                    <div class="modal-body">
+                        <form action="{{ route('navigation_store') }}" method="POST">
+                            @csrf
+
+
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-12 col-md-12">
+                                    <div class="form-group">
+                                        <strong>Name:</strong>
+                                        <input type="text" name="name" class="form-control" placeholder="Navigation Name" required>
+                                    </div>
+                                </div>
+                                <div class="col-xs-12 col-sm-12 col-md-12">
+                                    <div class="form-group">
+                                        <strong>Icon:</strong>
+                                        <input type="text" name="icon" class="form-control" placeholder="Navigation Icon">
+                                        {{--<select name="icon" id="icon" class="form-control">
+                            <option value="">Select Icon</option>
+                            <option value="fa fa-list">List<i class="fa fa-list"></i></option>
+                            <option value="fa fa-list">&#xf0f3;</option>
+                            <!-- <option value="0" selected="selected">No</option> -->
+                        </select>--}}
+                                    </div>
+                                </div>
+                                <div class="col-xs-12 col-sm-12 col-md-12">
+                                    <div class="form-group">
+                                        <strong>Sub Navigation:</strong>
+                                        <select name="sub_nav" id="sub_nav" class="form-control" onchange="hiden(value)">
+                                            <option value="1">Yes</option>
+                                            <option value="0" selected="selected">No</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-xs-12 col-sm-12 col-md-12">
+                                    <div class="form-group">
+                                        <strong>Is Show?:</strong>
+                                        <select name="is_show" id="is_show" class="form-control">
+                                            <option value="1" selected="selected">Yes</option>
+                                            <option value="0">No</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-xs-12 col-sm-12 col-md-12" id="route">
+                                    <div class="form-group">
+                                        <strong>URL:</strong>
+                                        <input type="text" name="route" class="form-control" placeholder="Enter URL">
+                                    </div>
+                                </div>
+                                <div class="col-xs-12 col-sm-12 col-md-12">
+                                    <div class="form-group">
+                                        <strong>Status:</strong>
+                                        <select name="status" id="status" class="form-control">
+                                            <option value="1" selected="selected">Yes</option>
+                                            <option value="0">No</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                {{--<!-- <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                        <button type="submit" class="btn btn-primary" style="margin-top: 10px;">Submit</button>
+                </div> -->--}}
+                            </div>
+
+                            <div class="modal-footer">
+                                <button type="submit" class="btn btn-success">Submit</button>
+                                <button type="button" class="btn btn-danger" id="edit_form" data-bs-dismiss="modal">Close</button>
+                            </div>
+                        </form>
+                    </div>
+
+                    {{--<!-- Modal footer -->--}}
+                    {{--<!-- <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                    </div> -->--}}
+
+                </div>
+            </div>
+        </div>
+        <div class="modal" id="edit_navigation">
+            <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+
+                    @php
+                    $title = "Edit Navigation";
+                    @endphp
+                    @section('title')
+                    {{ $title }}
+                    @endsection
+                    {{--<!-- Modal Header -->--}}
+                    <div class="modal-header">
+                        <h4 class="modal-title">{{ $title }}</h4>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                    </div>
+                    {{--<!-- Modal body -->--}}
+                    <div class="modal-body">
+                        <form action="{{ route('navigation_store') }}" method="POST">
+                            @csrf
+
+
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-12 col-md-12">
+                                    <div class="form-group">
+                                        <strong>Name:</strong>
+                                        <input type="text" name="name" id="nav_name" class="form-control" placeholder="Navigation Name" required>
+                                    </div>
+                                </div>
+                                <div class="col-xs-12 col-sm-12 col-md-12">
+                                    <div class="form-group">
+                                        <strong>Icon:</strong>
+                                        <input type="text" name="icon" id="nav_icon" class="form-control" placeholder="Navigation Icon">
+                                        {{--<select name="icon" id="icon" class="form-control">
+                            <option value="">Select Icon</option>
+                            <option value="fa fa-list">List<i class="fa fa-list"></i></option>
+                            <option value="fa fa-list">&#xf0f3;</option>
+                            <!-- <option value="0" selected="selected">No</option> -->
+                        </select>--}}
+                                    </div>
+                                </div>
+                                <div class="col-xs-12 col-sm-12 col-md-12">
+                                    <div class="form-group">
+                                        <strong>Sub Navigation:</strong>
+                                        <select name="sub_nav" id="nav_sub_nav" class="form-control" onchange="hiden(value)">
+                                            <option value="1">Yes</option>
+                                            <option value="0" selected="selected">No</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-xs-12 col-sm-12 col-md-12">
+                                    <div class="form-group">
+                                        <strong>Is Show?:</strong>
+                                        <select name="is_show" id="nav_is_show" class="form-control">
+                                            <option value="1" selected="selected">Yes</option>
+                                            <option value="0">No</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-xs-12 col-sm-12 col-md-12" id="route">
+                                    <div class="form-group">
+                                        <strong>URL:</strong>
+                                        <input type="text" name="route" id="nav_route" class="form-control" placeholder="Enter URL">
+                                    </div>
+                                </div>
+                                <div class="col-xs-12 col-sm-12 col-md-12">
+                                    <div class="form-group">
+                                        <strong>Status:</strong>
+                                        <select name="status" id="nav_status" class="form-control">
+                                            <option value="1" selected="selected">Active</option>
+                                            <option value="0">Inactive</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                {{--<!-- <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                        <button type="submit" class="btn btn-primary" style="margin-top: 10px;">Submit</button>
+                </div> -->--}}
+                            </div>
+
+                            <div class="modal-footer">
+                                <button type="submit" class="btn btn-success">Submit</button>
+                                <button type="button" class="btn btn-danger" id="edit_form" data-bs-dismiss="modal">Close</button>
+                            </div>
+                        </form>
+                    </div>
+
+                    {{--<!-- Modal footer -->--}}
+                    {{--<!-- <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                    </div> -->--}}
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="pcoded-main-container">
+        @if (count($errors) > 0)
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
+        <div class="pcoded-content">
+            <!-- [ breadcrumb ] start -->
+            <x-breadcrumb title="Navigations" :button="['name' => 'Add', 'allow' => true, 'id'=>'#add_navigation']" />
+            <!-- [ breadcrumb ] end -->
+            <!-- [ Main Content ] start -->
+            <div class="row">
+                <!-- product profit end -->
+
+                <div class="col-xl-12 col-md-12">
+                    <div class="card user-profile-list">
+                        <div class="card-body-dd theme-tbl">
+                            <x-table :keys="['Title', 'Icon', 'Sub Nav', 'Is Show?', 'Route', 'Status', 'Action']"></x-table>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    @include('layouts.dataTablesFiles')
+    @push('scripts')
+    <script>
+        $(document).ready(function() {
+            const datatable_url = route('navigation_index.datatable');
+            const datatable_columns = [{
+                    data: 'name'
+                },
+                {
+                    data: 'icon'
+                },
+                {
+                    data: 'sub_nav'
+                },
+                {
+                    data: 'is_show'
+                },
+                {
+                    data: 'route'
+                },
+                {
+                    data: 'action',
+                    width: '15%',
+                },
+                {
+                    data: 'sub_navigation',
+                    orderable: false,
+                    searchable: false
+                },
+
+            ];
+
+            create_datatables(datatable_url, datatable_columns);
+        });
+    </script>
+    @endpush
+</x-app-layout>
+<script type="text/javascript">
+    function hiden(value) {
+        if (value == 1) {
+            $('#route').css('display', 'none');
+        } else {
+            $('#route').css('display', 'block');
+        }
+
+    }
+</script>
+<script>
+    $(document).ready(function() {
+        $(document).on('click','#editNavigation', function(){
+            var nav_id = $(this).val();
+            // alert(nav_id);
+            // console.log(nav_id);
+            $('#edit_navigation').modal('show');
+            $.ajax({
+              type: "GET",
+              url:"navigation_edit/" + nav_id,
+              success: function (response){
+                // console.log(response.navigation[0].icon);
+                // console.log(nav_id);
+                // // $('#nav_id').val(nav_id);
+                $('#nav_name').val(response.navigation[0].name);
+                $('#nav_icon').val(response.navigation[0].icon);
+                $('#nav_sub_nav').val(response.navigation[0].sub_nav);
+                $('#nav_url').val(response.navigation[0].route);
+                $('#nav_status').val(response.navigation[0].is_active);
+              }
+            });
+        });
+        // $(document).on('click','#edit_form', function(){
+        //     // var nav_id = $(this).val();
+        //     // console.log(nav_id);
+        //     $('#editProduct').modal('ata-bs-dismiss');
+        // });
+    })
+    $(document).ready(function() {
+        $(document).on('click','#edit_form', function(){
+            // var nav_id = $(this).val();
+            // console.log(nav_id);
+            $('#edit_Product').modal('destroy');
+        });
+      })
+</script>
