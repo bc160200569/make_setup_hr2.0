da360b6c9c3f000b8b02b6df983a50063c4d88b5
bc160200569 <118533654+bc160200569@users.noreply.github.com>
2023-02-17 17:54:43 +0500

    add scope for uuid and protected fillable

--- app/Models/SubNavigation.php
+++ app/Models/SubNavigation.php
@@ -10,6 +10,46 @@ class SubNavigation extends Model
 {
     use HasFactory;
 
+    /**
+     * Scope a query to only include active users.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeUuid($query, $uuid)
+    {
+        return $query->where('uuid', $uuid);
+    }
+    
+    /**
+    * Get the route key for the model.
+    *
+    * @return string
+    */
+    public function getRouteKeyName()
+    {
+        return 'uuid';
+    }
+
+    /**
+     * boot
+     */
+    protected static function boot ()
+    {
+    	parent::boot();
+
+        static::creating(function ($model) {
+            $model->uuid = getUuid();
+        });
+        
+    }
+
+    protected $fillable = [
+        'name',
+        'route'
+    ];
+
+
     public function subnavrole()
     {
         return $this->belongsToMany(Role::class, 'role_has_sub_navigations');
